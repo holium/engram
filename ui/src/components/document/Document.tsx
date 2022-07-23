896249e66a46f1fbdb2f04f4ae75b64c540396df
Timmy <timothyhein@kryx.co>
2022-07-23 14:56:29 -0700

    a lot of bug fixes & some style updates from the revised figma

--- ui/src/components/document/Document.tsx
+++ ui/src/components/document/Document.tsx
@@ -7,29 +7,26 @@ import { EditorView } from "prosemirror-view";
 import schema from "./build/schema.ts";
 import { baseKeymap, buildKeymap } from "./build/keymap.ts";
 import dispatchTransaction from "./build/dispatchTransaction.ts";
-import { config } from "./plugins/config/plugin.ts"
+import { config } from "./plugins/config/plugin.ts";
 
 //Plugins
 import { collab } from "prosemirror-collab";
-import { history } from "prosemirror-history"
+import { history } from "prosemirror-history";
 import placeholders from "./plugins/placeholders.ts";
 import shortcuts from "./plugins/shortcuts.ts";
-
 
 // Menus
 import sidemenu from "./plugins/menus/sidemenu.ts";
 import SideMenu from "./plugins/menus/SideMenu.tsx";
 import highlightmenu from "./plugins/menus/highlightmenu.ts";
 import HighlightMenu from "./plugins/menus/HighlightMenu.tsx";
-import slashmenu from "./plugins/menus/slashmenu.ts"
-import NodeMenu from "./plugins/menus/NodeMenu.tsx"
-import ConfigMenu from "./plugins/config/ConfigMenu.tsx"
-import srcmenu from "./plugins/menus/srcmenu.ts"
-import SrcMenu from "./plugins/menus/SrcMenu.tsx"
-
+import slashmenu from "./plugins/menus/slashmenu.ts";
+import NodeMenu from "./plugins/menus/NodeMenu.tsx";
+import ConfigMenu from "./plugins/config/ConfigMenu.tsx";
+import srcmenu from "./plugins/menus/srcmenu.ts";
+import SrcMenu from "./plugins/menus/SrcMenu.tsx";
 
 function Document() {
-
   const [view, setView] = useState(null);
   const [showConfig, setConfig] = useState(false);
 
@@ -38,11 +35,6 @@ function Document() {
   const [srcMenu, setSrcMenu] = useState(null);
   const [nodeMenu, setNodeMenu] = useState(null);
   const [nodeMenuSearch, setNodeMenuSearch] = useState("");
-  function updateNodeMenu(search: string | null) {
-    console.log("updating search", search, nodeMenuSearch)
-    if(search == null) setNodeMenuSearch("")
-    else setNodeMenuSearch(`${nodeMenuSearch}${search}`)
-  }
   const [configMenu, setConfigMenu] = useState(null);
 
   function toggleConfig() {
@@ -68,7 +60,7 @@ function Document() {
         placeholders,
         sidemenu(setSideMenu),
         highlightmenu(setHighlightMenu),
-        slashmenu(setNodeMenu, updateNodeMenu),
+        slashmenu(setNodeMenu, setNodeMenuSearch),
         srcmenu(setSrcMenu),
         collab(),
         history({}),
@@ -84,56 +76,45 @@ function Document() {
 
   return (
     <div id="document-wrapper">
-
       {/* Document --------------------------------------------------------- */}
       <main id="document">
-      <section>
-        {sideMenu ? (
-          <SideMenu
-            menu={sideMenu}
-            hide={hideSideMenu}
-            view={view}
-          />
-        ) : (
-          ""
-        )}
-        {highlightMenu ? (
-          <HighlightMenu
-            menu={highlightMenu}
-            view={view}
-          />
-        ) : (
-          ""
-        )}
-        {srcMenu ? (
-          <SrcMenu
-            menu={srcMenu}
-            view={view}
-          />
-        ) : (
-          ""
-        )}
-        {nodeMenu ? (
-          <NodeMenu
-            menu={nodeMenu}
-            search={nodeMenuSearch}
-            hide={() => {setNodeMenu(null); updateNodeMenu(null);}}
-            view={view}
+        <section>
+          {sideMenu ? (
+            <SideMenu menu={sideMenu} hide={hideSideMenu} view={view} />
+          ) : (
+            ""
+          )}
+          {highlightMenu ? (
+            <HighlightMenu menu={highlightMenu} view={view} />
+          ) : (
+            ""
+          )}
+          {srcMenu ? <SrcMenu menu={srcMenu} view={view} /> : ""}
+          {nodeMenu ? (
+            <NodeMenu
+              menu={nodeMenu}
+              search={nodeMenuSearch}
+              hide={() => {
+                setNodeMenu(null);
+                setNodeMenuSearch("");
+              }}
+              view={view}
             />
-        ) :(
-          ""
-        )}
-        {configMenu ? (
-          <ConfigMenu
-            menu={configMenu}
-            hide={() => {setConfigMenu(null);}}
-            view={view}
+          ) : (
+            ""
+          )}
+          {configMenu ? (
+            <ConfigMenu
+              menu={configMenu}
+              hide={() => {
+                setConfigMenu(null);
+              }}
+              view={view}
             />
-        ) :(
-          ""
-        )}
-      </section>
-
+          ) : (
+            ""
+          )}
+        </section>
       </main>
     </div>
   );
--- ui/src/components/document/plugins/menus/SideMenu.tsx
+++ ui/src/components/document/plugins/menus/SideMenu.tsx
@@ -1,30 +1,28 @@
-import { useState, useRef, useEffect } from "react"
-import NodeMenu from "./NodeMenu.tsx"
-import { TextSelection } from "prosemirror-state"
-import { toggleMark } from "prosemirror-commands"
-import schema from "../../build/schema.ts"
-import { insertAtNextPossible } from "../shortcuts.ts"
+import { useState, useRef, useEffect } from "react";
+import NodeMenu from "./NodeMenu.tsx";
+import { TextSelection } from "prosemirror-state";
+import { toggleMark } from "prosemirror-commands";
+import schema from "../../build/schema.ts";
+import { insertAtNextPossible } from "../shortcuts.ts";
 
 function SideMenu(props) {
-
-  const menuButton = useRef()
+  const menuButton = useRef();
   const [nodeMenu, setNodeMenu] = useState(null);
-  const [pos, setPos] = useState(props.menu.pos)
+  const [pos, setPos] = useState(props.menu.pos);
 
   useEffect(() => {
-    setPos(props.menu.pos)
-  }, [props.menu.pos])
+    setPos(props.menu.pos);
+  }, [props.menu.pos]);
 
   function openNodeMenu(event) {
     const parent = document.querySelector(".sidemenu").getBoundingClientRect();
-    console.log(event)
     setNodeMenu({
       node: props.menu.node,
       to: props.menu.pos,
       from: props.menu.pos,
       left: event.clientX - parent.left,
-      top: event.clientY - parent.top
-    })
+      top: event.clientY - parent.top,
+    });
   }
 
   function handleDrag() {
@@ -32,26 +30,39 @@ function SideMenu(props) {
   }
 
   function handleDragEnd(event) {
-    const cursor = props.view.posAtCoords({left: event.clientX, top: event.clientY});
-    if(cursor && cursor.pos != pos) {
-      const move = insertAtNextPossible(props.view, cursor.pos, props.menu.node);
-      if(move != null) {
+    const cursor = props.view.posAtCoords({
+      left: event.clientX,
+      top: event.clientY,
+    });
+    if (cursor && cursor.pos != pos) {
+      const move = insertAtNextPossible(
+        props.view,
+        cursor.pos,
+        props.menu.node
+      );
+      if (move != null) {
         const prev = move.tr.mapping.map(pos);
         //const cleanup = props.view.state.tr.setSelection(new TextSelection(props.view.state.doc.resolve(prev + 1), props.view.state.doc.resolve(prev + props.menu.node.nodeSize - 1)))
-        const cleanup = props.view.state.tr.deleteRange(prev, prev + props.menu.node.nodeSize)
-        props.view.dispatch(cleanup)
+        const cleanup = props.view.state.tr.deleteRange(
+          prev,
+          prev + props.menu.node.nodeSize
+        );
+        props.view.dispatch(cleanup);
         //toggleMark(schema.marks["strong"])(props.view.state, props.view.dispatch, props.view)
-        setPos(move.pos)
+        setPos(move.pos);
       }
     }
   }
 
-
   return (
-    <menu className="sidemenu" style={{
-      top: `${props.menu.top}px`,
-      position: "absolute"
-    }} onMouseLeave={props.hide}>
+    <menu
+      className="sidemenu"
+      style={{
+        top: `${props.menu.top}px`,
+        position: "absolute",
+      }}
+      onMouseLeave={props.hide}
+    >
       {/* Plus */}
       <li onClick={openNodeMenu}>
         <svg
@@ -67,7 +78,14 @@ function SideMenu(props) {
         </svg>
       </li>
       {nodeMenu ? (
-        <NodeMenu menu={nodeMenu} view={props.view} pos={props.menu.pos} hide={() => {setNodeMenu(null)}} />
+        <NodeMenu
+          menu={nodeMenu}
+          view={props.view}
+          pos={props.menu.pos}
+          hide={() => {
+            setNodeMenu(null);
+          }}
+        />
       ) : (
         ""
       )}
