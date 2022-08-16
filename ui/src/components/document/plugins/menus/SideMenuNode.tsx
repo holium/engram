2ccfe590a130abee432205af22269e3a3e3552bc
Timmy <timmy0x@proton.me>
2022-08-16 12:17:46 -0700

    some more style fixes

--- ui/src/components/document/plugins/menus/SideMenuNode.tsx
+++ ui/src/components/document/plugins/menus/SideMenuNode.tsx
@@ -92,6 +92,7 @@ function SideMenu(props) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
+          fill="var(--type-color)"
           className="icon clickable"
         >
           <path fill="none" d="M0 0h24v24H0z" />
