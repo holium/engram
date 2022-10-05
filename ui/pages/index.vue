bfeb8a9c96f0ad22581bd0739683483d7d139dfa
Timmy <timmy0x@proton.me>
2022-10-05 13:01:56 -0700

    init vue ui:

---
+++ ui/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Engram UI Refactor</h1>
+    <ul>
+      <li>Build a <strong>tight</strong>, minimal interface</li>
+      <li>Create a delightful document experience</li>
+      <li>
+        Effectively demonstrate the an MVP of a core atomic, extendable, crdt
+        for urbit products
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
