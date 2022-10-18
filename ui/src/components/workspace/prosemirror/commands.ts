18a2a815554912a6349b0b705d90b80a04896bea
Timmy <timmy0x@proton.me>
2022-10-18 06:21:57 -0700

    added in the keymap & keyboard shortcuts

---
+++ ui/src/components/workspace/prosemirror/commands.ts
@@ -0,0 +1,59 @@
+import {
+  EditorState,
+  Transaction,
+  AllSelection,
+  TextSelection,
+} from "prosemirror-state";
+import type { Command } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import type { NodeType, ContentMatch } from "prosemirror-model";
+import schema from "./schema";
+
+export const hardBreak: Command = function (state, dispatch, view) {
+  if (dispatch) {
+    const sel = state.selection;
+    const tr = state.tr.insert(sel.to, schema.nodes["hard-break"].create());
+    dispatch(tr);
+    return true;
+  }
+  return false;
+};
+
+export const createNodeNear: (node: NodeType, pos?: number) => Command =
+  (node, pos) => (state, dispatch?) => {
+    const sel = state.selection,
+      { $from, $to } = sel;
+
+    if (!node) return false;
+    if (dispatch) {
+      const side = pos
+        ? pos
+        : (!$from.parentOffset && $to.index() < $to.parent.childCount
+            ? $from
+            : $to
+          ).pos;
+      const tr = state.tr.insert(side, node.createAndFill()!);
+      tr.setSelection(TextSelection.create(tr.doc, side + 1));
+      dispatch(tr.scrollIntoView());
+    }
+    return true;
+  };
+
+export const stopTab: Command = function (state, dispatch, view) {
+  /*
+  if (dispatch) {
+    return true;
+  }
+  return false;
+  */
+  return true;
+};
+
+// helpers
+function defaultBlockAt(match: ContentMatch) {
+  for (let i = 0; i < match.edgeCount; i++) {
+    const { type } = match.edge(i);
+    if (type.isTextblock && !type.hasRequiredAttrs()) return type;
+  }
+  return null;
+}
--- ui/src/components/workspace/prosemirror/render.ts
+++ ui/src/components/workspace/prosemirror/render.ts
@@ -2,13 +2,22 @@ import { EditorView } from "prosemirror-view";
 import { EditorState } from "prosemirror-state";
 
 import schema from "./schema";
+import keymap from "./keymap";
+import shortcuts from "./shortcuts";
+import save from "./save";
 
 export default function (workspace: any, place: HTMLElement): EditorView {
   console.log(place);
   console.log(workspace);
   const state = EditorState.create({
     schema: schema,
-    plugins: [],
+    plugins: [
+      keymap,
+      shortcuts,
+      save((view) => {
+        console.log("Saving...");
+      }),
+    ],
   });
   const view = new EditorView(place, {
     state,
