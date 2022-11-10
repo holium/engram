101d4727b30422b33085032637b7e6676499f520
Timmy <timmy0x@proton.me>
2022-11-09 22:26:00 -0500

    new folder / document button on workspace

--- ui/src/components/Toolbar.vue
+++ ui/src/components/Toolbar.vue
@@ -3,8 +3,6 @@
     <!-- Nav Show / hide -->
     <div class="p-2">
       <svg
-        width="16"
-        height="16"
         viewBox="0 0 16 16"
         fill="var(--rlm-icon-color, #333333)"
         class="icon clickable nav-toggle"
@@ -20,8 +18,6 @@
     <!-- Dock Show / hide -->
     <div class="p-2">
       <svg
-        width="16"
-        height="16"
         viewBox="0 0 16 16"
         fill="var(--rlm-icon-color, #333333)"
         class="icon clickable dock-toggle"
--- ui/src/components/navbar/FileSystem.vue
+++ ui/src/components/navbar/FileSystem.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="flex flex-col">
-        <div class="heading px-3 py-2">
-            workspace
+        <div class="heading px-3 py-2 flex" @contextmenu="openMenu">
+            <div class="flex-grow">
+                workspace
+            </div>
+            <svg 
+                @click="openMenu"
+                class="icon clickable"
+                viewBox="0 0 16 16" 
+                fill="var(--rlm-icon-color, #333333)"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path d="M2.66667 2H13.3333C13.5101 2 13.6797 2.07024 13.8047 2.19526C13.9298 2.32029 14 2.48986 14 2.66667V13.3333C14 13.5101 13.9298 13.6797 13.8047 13.8047C13.6797 13.9298 13.5101 14 13.3333 14H2.66667C2.48986 14 2.32029 13.9298 2.19526 13.8047C2.07024 13.6797 2 13.5101 2 13.3333V2.66667C2 2.48986 2.07024 2.32029 2.19526 2.19526C2.32029 2.07024 2.48986 2 2.66667 2ZM3.33333 3.33333V12.6667H12.6667V3.33333H3.33333ZM7.33333 7.33333V4.66667H8.66667V7.33333H11.3333V8.66667H8.66667V11.3333H7.33333V8.66667H4.66667V7.33333H7.33333Z" />
+            </svg>            
         </div>
         <SystemItem :item="item" :type="'document'" :key="item.id" v-for="item in documents" />
     </div>
@@ -12,11 +23,13 @@ import { defineComponent } from "vue";
 import store from "@/store/index";
 import SystemItem from "./SystemItem.vue";
 import type { DocumentMeta, FolderMeta } from "@/store/types";
+import { Menu } from "@/components/menus/types";
 export default defineComponent({
     name: "FileSystem",
     components: {
         SystemItem,
     },
+    inject: ["pushMenu"],
     created: function() {
         store.dispatch("documents/load");
     },
@@ -27,6 +40,27 @@ export default defineComponent({
         folders: function(): Array<FolderMeta> {
             //return store.getters['folder/list'];
             return [];
+        }
+    },
+    methods: {
+        openMenu: function(event: MouseEvent) {
+            (this as any).pushMenu(new Menu({ top: event.clientY, left: event.clientX}, [
+                {
+                    display: "New Folder",
+                    icon: "",
+                    run: () => {
+                        store.dispatch("documents/make", { name: "Untitled Document" })
+                    }
+                },
+                {
+                    display: "New Document",
+                    icon: "",
+                    run: () => {
+                        store.dispatch("folders/make", { name: "Untitled Folder" })
+                    }
+
+                }
+            ]))
         }
     }
 })
