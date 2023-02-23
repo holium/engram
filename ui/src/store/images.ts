6d9fcc2445bcb59df394581b8af19aafa73f3435
Timmy <timmy0x@proton.me>
2023-02-23 08:40:39 -0500

    removing logs

--- ui/src/store/images.ts
+++ ui/src/store/images.ts
@@ -1,64 +1,5 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-/*
-export enum StorageAcl {
-    PublicRead = 'public-read',
-}
-  
-export interface UploadParams {
-    Bucket: string; // the bucket to upload the object to
-    Key: string; // the desired location within the bucket
-    ContentType: string; // the object's mime-type
-    ACL: StorageAcl; // ACL, always 'public-read'
-    Body: File; // the object itself
-}
-
-export interface UploadResult {
-Location: string;
-}
-
-// Extra layer of indirection used by S3 client.
-export interface StorageUpload {
-promise: () => Promise<UploadResult>;
-}
-
-export interface StorageClient {
-upload: (params: UploadParams) => StorageUpload;
-}
-
-export class S3Client implements StorageClient {
-    config: S3.ClientConfiguration;
-    client: S3 | null = null;
-    S3!: typeof import('aws-sdk/clients/s3');
-  
-    constructor(config: S3.ClientConfiguration) {
-      this.config = config;
-    }
-  
-    async initAndUpload(params: UploadParams) {
-      if (!this.S3) {
-        await this.loadLib();
-      }
-  
-      if (!this.client) {
-        this.client = new this.S3(this.config);
-      }
-  
-      return await this.client.upload(params).promise();
-    }
-  
-    upload(params: UploadParams): StorageUpload {
-      const upload = this.initAndUpload.bind(this);
-      return {
-        promise: async () => await upload(params),
-      };
-    }
-  
-    async loadLib() {
-      this.S3 = (await import('aws-sdk/clients/s3')).default;
-    }
-  */
-
 export let s3storeclient: null | S3Client = null;
 export let s3bucket = "";
 export let s3endpoint = ""
@@ -66,8 +7,6 @@ export let s3endpoint = ""
 export function loadS3() {
     (window as any).urbit.scry({ app: "s3-store", path: "/configuration" }).then((config: any) => {
         (window as any).urbit.scry({ app: "s3-store", path: "/credentials" }).then((creds: any) => {
-            console.log("config res: ", config);
-            console.log("cred res: ", creds);
             if(config["s3-update"].configuration.currentBucket.length > 0) {
                 s3storeclient = new S3Client({
                     endpoint: creds["s3-update"].credentials.endpoint, 
